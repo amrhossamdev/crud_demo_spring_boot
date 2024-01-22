41d511490500c21f43e808edb3f96f1b6c7ba01d
Amr Hossam <amrhossam.dev@gmail.com>
2024-01-22 14:43:16 +0200

    Added query for saving student

--- src/main/java/com/amr/cruddemo/Application.java
+++ src/main/java/com/amr/cruddemo/Application.java
@@ -1,5 +1,7 @@
 package com.amr.cruddemo;
 
+import com.amr.cruddemo.dao.StudentDAO;
+import com.amr.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,24 @@ public class Application {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("Hello world");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+
+        // create the student object
+        System.out.println("Create new student");
+
+        Student temp = new Student("Amr", "Hossam", "amrhossam.dev@gmail.com");
+        // save the student object
+        System.out.println("Saving....");
+        studentDAO.save(temp);
+
+        // display id of the saved student
+        System.out.println("Done saved" + temp.getId());
+
     }
 }
